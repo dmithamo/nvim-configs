a93fe73ae2635d06c25c702882c6c9743b3b9f3a
dmithamo <33679184+dmithamo@users.noreply.github.com>
2026-05-14 17:44:12 +0300

    chore: expt with transparency and give up

--- ftplugin/java.lua
+++ ftplugin/java.lua
@@ -3,18 +3,19 @@ if not status then
   return
 end
 
-local nvlsp = require("nvchad.configs.lspconfig")
+local nvlsp = require "nvchad.configs.lspconfig"
 
-local home = os.getenv("HOME")
+local home = os.getenv "HOME"
 local project_name = vim.fn.fnamemodify(vim.fn.getcwd(), ":p:h:t")
 local workspace_dir = home .. "/.local/share/nvim/jdtls-workspace/" .. project_name
 
 local config = {
   cmd = {
     "jdtls",
-    "-data", workspace_dir,
+    "-data",
+    workspace_dir,
   },
-  root_dir = require("jdtls.setup").find_root({ ".git", "mvnw", "gradlew", "pom.xml", "build.gradle" }),
+  root_dir = require("jdtls.setup").find_root { ".git", "mvnw", "gradlew", "pom.xml", "build.gradle" },
   on_attach = nvlsp.on_attach,
   capabilities = nvlsp.capabilities,
   settings = {
@@ -24,21 +25,7 @@ local config = {
         organizeImports = true,
       },
       configuration = {
-        runtimes = {
-          {
-            name = "JavaSE-11",
-            path = home .. "/.sdkman/candidates/java/11.0.21-tem",
-          },
-          {
-            name = "JavaSE-17",
-            path = home .. "/.sdkman/candidates/java/17.0.9-tem",
-            default = true,
-          },
-          {
-            name = "JavaSE-21",
-            path = home .. "/.sdkman/candidates/java/21.0.1-tem",
-          }
-        }
+        runtimes = {},
       },
       completion = {
         favoriteStaticMembers = {
