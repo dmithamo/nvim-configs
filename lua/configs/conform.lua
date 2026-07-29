98422259ef284722cb90b7a5aac23fc6897a8d35
dmithamo <33679184+dmithamo@users.noreply.github.com>
2026-07-29 14:27:28 +0300

    support toml formatting

--- lua/configs/conform.lua
+++ lua/configs/conform.lua
@@ -1,6 +1,7 @@
 local options = {
   formatters_by_ft = {
     lua = { "stylua" },
+    toml = { "taplo" },
 
     -- Dynamically choose between deno_fmt and dprint
     javascript = function(bufnr)
